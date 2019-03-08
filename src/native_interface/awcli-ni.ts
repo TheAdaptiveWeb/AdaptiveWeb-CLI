ac135a60906211b225924db61b8e9e57bcc62fb1
Adam Hirst <adam@ahirst.com>
2019-03-08 15:13:37 +0000

    Removing native interfaces in favour of WebSockets

--- src/native_interface/awcli-ni.ts
+++ src/native_interface/awcli-ni.ts
@@ -5,6 +5,19 @@ import * as fs from 'fs';
 const AWCLI_NI_ROOT = process.env.HOME + '/.adaptiveweb/developer';
 const AWCLI_NI_WATCH_LOCATION = AWCLI_NI_ROOT + '/dev_adapters';
 
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const port = 13551;
+
+http.listen(port, () => { console.log('Development server listening on port ' + port); });
+
+process.stdin.resume();
+process.stdin.on('end', () => {
+    process.exit();
+});
+
 function createIfNonExistant(path: string) {
     if (!fs.existsSync(path)) fs.mkdirSync(path, { recursive: true });
 }
@@ -12,14 +25,16 @@ function createIfNonExistant(path: string) {
 createIfNonExistant(AWCLI_NI_WATCH_LOCATION);
 
 // Start watching
-try {
-    fs.watch(AWCLI_NI_WATCH_LOCATION, (event, filename) => {
+
+fs.watch(AWCLI_NI_WATCH_LOCATION, (event, filename) => {
+    try {
         if (filename.endsWith('.json')) {
             // Load file
             let path = AWCLI_NI_WATCH_LOCATION + '/' + filename;
             if (!fs.existsSync(path)) return;
             let raw: string = fs.readFileSync(path, 'utf8');
-            console.log(raw);
+            let json: any = JSON.parse(raw);
+            io.local.emit('adapterUpdate', json);
         }
-    });
-} catch (ex) { console.error(ex); }
+    } catch (ex) { console.error(ex); }
+});
