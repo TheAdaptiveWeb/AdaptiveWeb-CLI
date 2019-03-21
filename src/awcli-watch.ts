04f0776751fd10e7df83fe9abe8b3dc42f43c021
Adam Hirst <adam@ahirst.com>
2019-03-21 14:21:52 +0000

    Fixed would reload config upon change; closes #5

--- src/awcli-watch.ts
+++ src/awcli-watch.ts
@@ -3,22 +3,37 @@ import * as Builder from './tasks/BuildHelpers';
 import { getConfig } from './tasks/LocateConfig';
 import { startServer } from './native_interface/awcli-ni';
 import { devModeWarning, watchingFileChanges } from './tasks/Messages';
+import * as colors from 'colors';
 const watch: any = require('node-watch');
 
 const AWCLI_NI_ROOT = process.env.HOME + '/.adaptiveweb/developer';
 const AWCLI_NI_WATCH_LOCATION = AWCLI_NI_ROOT + '/dev_adapters';
 
+function log(message: string) {
+    let formatTime = (x: any) => { if ((''+x).length < 2) x='0'+x; return x; }
+    let dt = new Date();
+    let time = colors.green(`${formatTime(dt.getHours())}:${formatTime(dt.getMinutes())}:${formatTime(dt.getSeconds())}`);
+    let msg = `[${time}] ${message}`;
+    console.log(msg);
+}
+
+console.clear();
 console.log(devModeWarning);
 
 let { dir, config } = getConfig();
 
-let awconfig = JSON.parse(fs.readFileSync(config, 'utf8'));
-awconfig.webpackConfig = dir + '/' + awconfig.webpackConfig;
+let awconfig: any;
+function loadConfig() {
+    awconfig = JSON.parse(fs.readFileSync(config, 'utf8'));
+    awconfig.webpackConfig = dir + '/' + awconfig.webpackConfig;
+}
+loadConfig();
 
-process.stdout.write(watchingFileChanges);
+log(watchingFileChanges);
 Builder.build(awconfig, AWCLI_NI_WATCH_LOCATION);
+log('Adapter ' + colors.bold(awconfig.id) + ' compilation successful!');
 
-startServer();
+startServer(() => { log('Development server started on port 13551.'); });
 
 watch(dir, { recursive: true }, (event: any, filename: string) => {
     try {
@@ -26,7 +41,10 @@ watch(dir, { recursive: true }, (event: any, filename: string) => {
         if (filename.startsWith(dir + '/dist')) return;
         if (filename.startsWith(dir + '/node_modules')) return;
 
-        process.stdout.write(watchingFileChanges + '\nFile change detected: ' + filename + '; Rebuilding');
+        if (filename.endsWith('awconfig.json')) loadConfig();
+        
+        log('File change detected: ' + filename + '; Rebuilding');
         Builder.build(awconfig, AWCLI_NI_WATCH_LOCATION);
+        log('Adapter ' + colors.bold(awconfig.id) + ' compilation successful!');
     } catch (ex) { console.error(ex); }
 });
