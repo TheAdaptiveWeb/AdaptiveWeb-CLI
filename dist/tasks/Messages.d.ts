3649acdd8da809f4c139af2438b7f9f4dbd502ef
Adam Hirst <adam@ahirst.com>
2019-03-27 15:44:53 +0000

    Added dist

--- dist/tasks/Messages.d.ts
+++ dist/tasks/Messages.d.ts
@@ -1,3 +1,17 @@
+/**
+ *  Copyright 2019 The Adaptive Web. All Rights Reserved.
+ *
+ *  Licensed under the Mozilla Public License 2.0 (the "License").
+ *  You may not use this file except in compliance with the License.
+ *  A copy of the License is located at
+ *
+ *      https://www.mozilla.org/en-US/MPL/2.0/
+ *
+ *  or in the "license" file accompanying this file. This file is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
 export declare const introduction = "This utility will walk you through the creation of a awconfig.json file.\n\nIf you want to use preferences in your project, you will need to specify them\nthe awconfig.json file once you have completed this utility. See\n[insert documentation link here] for details.\n\nPress ^C at any time to quit.";
 export declare const nextSteps: string;
 export declare const configWritten: string;
